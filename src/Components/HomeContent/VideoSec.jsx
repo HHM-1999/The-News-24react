bd49678bc9b0a10971e531edc4ab4b14d0e66c89
HHM-1999 <hhm639069@gmail.com>
2024-05-21 12:24:40 +0600

    Video section fixed + Google tag added

--- src/Components/HomeContent/VideoSec.jsx
+++ src/Components/HomeContent/VideoSec.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { scrollTop, ForLazyLoaderImg } from '../AllFunctions'
@@ -8,6 +8,9 @@ import Slider from 'react-slick';
 var lazyloaded = false
 export default function VideoSec() {
     const [videos, setVideos] = useState([])
+    const [slideIndex, setSlideIndex] = useState(0);
+    const [updateCount, setUpdateCount] = useState(0);
+    let sliderRef = useRef(null);
     useEffect(() => {
         axios
             .get(`${process.env.REACT_APP_API_URL}json/file/generatePositionVideoCategory1.json`)
@@ -21,16 +24,23 @@ export default function VideoSec() {
     }, [])
 
     var settings = {
+        dots: false,
+        infinite: true,
+        speed: 100,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        afterChange: () => setUpdateCount(updateCount + 1),
+        beforeChange: (current, next) => setSlideIndex(next),
         centerPadding: "50px",
         className: "center",
         centerMode: true,
-        dots: false,
-        infinite: true,
-        arrows: true,
-        autoplay: true,
+        // dots: true,
+        // infinite: true,
+        // arrows: true,
+        // autoplay: true,
         pauseOnFocus: true,
         Speed: 100,
-        slidesToShow: 4,
+        // slidesToShow: 4,
         slidesToScroll: 1,
         initialSlide: 0,
         responsive: [
@@ -71,46 +81,50 @@ export default function VideoSec() {
 
                 </div>
                 <div className="slider-container">
-               
+
                     <div className="row Dmargin">
-                       
-                    <Slider {...settings}>
+
+                        <Slider ref={slider => {
+                            sliderRef = slider;
+                        }}
+                            {...settings}>
                             {videos.map((nc) => {
                                 return (
-                                        <div className="DMoreVideoListItem   " key={nc.WebTVID} style={{padding:"0 10px",margin:"0 10px"}}>
-                                            <Link to={"/video/show/" + nc.WebTVID} onClick={scrollTop}>
-                                                <div className="DMoreVideoThumb">
-                                                    <div className="Imgresize">
-                                                        <figure className="ImgViewer">
-                                                            <picture className="FixingRatio">
-                                                                <img src={process.env.REACT_APP_LAZYL_IMG} data-src={'https://img.youtube.com/vi/' + nc.WebTVLinkCode + '/0.jpg'} width={406} height={228} alt={nc.WebTVHeading} title={nc.WebTVHeading} className="img100 ImgRatio" />
-                                                                <div className="card-video-icon big transition"> <i className="fa-solid fa-play"></i> </div>
-                                                            </picture>
-                                                        </figure>
-                                                    </div>
-                                                    <div className="card-video-img transition"></div>
+                                    <div className="DMoreVideoListItem   " key={nc.WebTVID} style={{ padding: "0 10px", margin: "0 10px" }}>
+                                        <Link to={"/video/show/" + nc.WebTVID} onClick={scrollTop}>
+                                            <div className="DMoreVideoThumb">
+                                                <div className="Imgresize">
+                                                    <figure className="ImgViewer">
+                                                        <picture className="FixingRatio">
+                                                            <img src={process.env.REACT_APP_LAZYL_IMG} data-src={'https://img.youtube.com/vi/' + nc.WebTVLinkCode + '/0.jpg'} width={406} height={228} alt={nc.WebTVHeading} title={nc.WebTVHeading} className="img100 ImgRatio" />
+                                                            <div className="card-video-icon big transition"> <i className="fa-solid fa-play"></i> </div>
+                                                        </picture>
+                                                    </figure>
                                                 </div>
-                                                <div className="Desc">
-                                                    <h3 className="Title">{nc.WebTVHeading}</h3>
-                                                </div>
-                                            </Link>
-                                        </div>
-                             
+                                                <div className="card-video-img transition"></div>
+                                            </div>
+                                            <div className="Desc">
+                                                <h3 className="Title">{nc.WebTVHeading}</h3>
+                                            </div>
+                                        </Link>
+                                    </div>
+
 
 
                                 )
                             })}
-                         </Slider>
+                        </Slider>
 
                     </div>
-                 
+
 
 
                 </div>
 
- 
-            </div>
 
+            </div>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/react-slick/0.30.2/react-slick.min.js" integrity="sha512-9cn+e5E0uqJxF/RA4PERrZ9654f/pPwSWJWe1fPavysxUdVe0xYTj09jzlKW5pd9vrgaNtqDpW9CwkvKXdUBSw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/react-slick/0.30.2/react-slick.js" integrity="sha512-aP0zyfgfU02K5gDdprwgUcfmGQJ7zoVQPdIl1irO8iXCUJ80yWeTg/WTRwTJuV0pny/jWLZqBZwhz8pkb6NxlA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
         </>
 
