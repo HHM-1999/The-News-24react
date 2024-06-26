a7ee0a924c088b83016130199378764c0a65994f
HHM-1999 <hhm639069@gmail.com>
2024-06-26 13:39:43 +0600

    Amp all segments hide

--- src/RouterLink.jsx
+++ src/RouterLink.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainRouterLink from './MainRouterLink'
-import AmpRouterLink from './AmpRouterLink';
+
 
 export default function RouterLink() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/*" element={<MainRouterLink />} />
-                <Route path="/amp/*" element={<AmpRouterLink />} />
             </Routes>
         </BrowserRouter>
     )
